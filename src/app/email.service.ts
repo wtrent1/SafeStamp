fb3fb09d4b058a13f8ce7680828c0989519d56cd
Chris McKnight <cmckni3@gmail.com>
2018-05-28 13:51:03 -0500

    fix(email): Fix email URL and add CORS

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -9,14 +9,14 @@ export class EmailService {
 
   constructor(private http: HttpClient) { }
 
-  sendEmail(emailBody) {
-    // @cmckni3 Your node.js server and ng application will be running at different
-    // locations in development so a relative URL won't work here
-    // This could be changed to https://localhost:3000
-    // for now but that won't work in production and it's not configurable
+  // @cmckni3 Your node.js server and ng application will be running at different
+  // locations in development so a relative URL won't work here
+  // This could be changed to http://localhost:3000
+  // for now but that won't work in production and it's not configurable
 
-    // @cmckni3 Removed httpOptions as well since HttpClient sets that as the default
-    // Content-Type and sends JSON by default
-    return this.http.post('https://localhost:3000', emailBody);
+  // @cmckni3 Removed httpOptions as well since HttpClient sets that as the default
+  // Content-Type and sends JSON by default
+  sendEmail(emailBody) {
+    return this.http.post('http://localhost:3000/api/email', emailBody);
   }
 }
