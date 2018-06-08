b66563e1ee16bca66ca86a39cb157745bb1c6b38
John Lea <j.r.lea83@gmail.com>
2018-06-07 21:36:50 -0500

    added page refresh

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -24,6 +24,7 @@ export class ContactComponent implements OnInit {
 
       success => {
         console.log(success);
+        window.location.reload();
         alert('Email Sent!');
       });
   }
