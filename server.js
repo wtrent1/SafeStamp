d2091142dcf7df502439662564e5784499a75768
Walker Trent <walktrent@gmail.com>
2018-10-24 19:01:34 -0500

    Changed corsoptions in server.js spelling is hard

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/dist/SafeStamp-Final'));
 app.use(bodyParser.json()); // support json encoded bodies
 
 var corsOptions = {
-  origin: 'https://safeestamp.herokuapp.com',
+  origin: 'www.safestamp.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
